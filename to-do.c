48c80b51363fce1c9d820907d987bf6b94495fca
Guadalupe Mendoza <cuki13lucky@gmail.com>
2026-04-21 14:56:52 -0300

    avance en la interfaz

--- to-do.c
+++ to-do.c
@@ -16,11 +16,24 @@ typedef struct Nodo{
 } Nodo;
 
 Nodo * CrearListaVacia();
-Nodo * CrearNodo(int tarea, char * descripcion, int duracion);
+Nodo * CrearNodo(int tarea, /*char * descripcion,*/ int duracion);
 
 int main(){
     Nodo * TareasPendientes;
     TareasPendientes=CrearListaVacia();
+    srand(time(NULL));
+
+    int opcion;
+    printf("\nElija la opción de la tarea a realizar: \n\n1- Ingresar una nueva tarea pendiente. \n2- Marcar una tarea como completada.\n3- Chequear la lista de tareas pendientes.\n");
+    scanf("%d", &opcion);
+    if (opcion==1){
+        printf("\n\n========================\n\n====NUEVA TAREA PENDIENTE====\n========================\n");
+       /* printf("\nIngrese su nombre: ")*/
+        int tarea_duracion=10+rand()%100;
+        int tarea_id=1000+rand();
+        TareasPendientes=CrearNodo(tarea_id, tarea_duracion);
+    }
+    
 
 }
 
@@ -28,9 +41,12 @@ Nodo * CrearListaVacia(){
     return NULL;
 }
 
-Nodo * CrearNodo(int tarea, char * descripcion, int duracion){
+Nodo * CrearNodo(int tarea, /*char * descripcion,*/ int duracion){
     Nodo * nuevoNodo= (Nodo *)malloc(sizeof(Nodo));
     nuevoNodo->T.TareaID=tarea;
     nuevoNodo->T.Duracion=duracion;
-    char buffer[MAX];
+    /*char buffer[MAX];*/
+    nuevoNodo->Siguiente=NULL;
+
+    return nuevoNodo;
 }
