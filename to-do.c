c0b4326b64642d7c426209202193ba01f6e07599
Guadalupe Mendoza <cuki13lucky@gmail.com>
2026-04-21 15:32:38 -0300

    Correcciones

--- to-do.c
+++ to-do.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define MAX 100;
+#define MAX 100
 
 typedef struct Tarea{
     int TareaID;//Numérico autoincremental comenzando en 1000
@@ -12,26 +12,37 @@ typedef struct Tarea{
 
 typedef struct Nodo{
     Tarea T;
-    Nodo *Siguiente;
+    struct Nodo * Siguiente;
 } Nodo;
 
 Nodo * CrearListaVacia();
-Nodo * CrearNodo(int tarea, /*char * descripcion,*/ int duracion);
+Nodo * CrearNodo(int tarea, char * descripcion, int duracion);
+void InsertarNodo(Nodo ** cabeza, Nodo *nuevoNodo);
 
 int main(){
-    Nodo * TareasPendientes;
-    TareasPendientes=CrearListaVacia();
+    Nodo * C_TareasPendientes, *TareasPendientes;
+    C_TareasPendientes=CrearListaVacia();
     srand(time(NULL));
 
     int opcion;
     printf("\nElija la opción de la tarea a realizar: \n\n1- Ingresar una nueva tarea pendiente. \n2- Marcar una tarea como completada.\n3- Chequear la lista de tareas pendientes.\n");
     scanf("%d", &opcion);
+    char buffer[MAX];
+    int ultimoID=999;
     if (opcion==1){
-        printf("\n\n========================\n\n====NUEVA TAREA PENDIENTE====\n========================\n");
+        printf("\n\n========================\n NUEVA TAREA PENDIENTE \n========================\n");
        /* printf("\nIngrese su nombre: ")*/
+        printf("Ingrese la descripción de la tarea: ");
+        scanf("%s", buffer);
+        int lenght_buffer=strlen(buffer);
+        char *descripcion= (char *) malloc(sizeof(char)*(lenght_buffer+1));
+        strcpy(descripcion, buffer);
         int tarea_duracion=10+rand()%100;
-        int tarea_id=1000+rand();
-        TareasPendientes=CrearNodo(tarea_id, tarea_duracion);
+        ultimoID++;
+        int tarea_id=ultimoID;
+        TareasPendientes=CrearNodo(tarea_id, descripcion, tarea_duracion);
+        InsertarNodo(&C_TareasPendientes, TareasPendientes);
+        
     }
     
 
@@ -41,12 +52,17 @@ Nodo * CrearListaVacia(){
     return NULL;
 }
 
-Nodo * CrearNodo(int tarea, /*char * descripcion,*/ int duracion){
+Nodo * CrearNodo(int tarea, char * descripcion, int duracion){
     Nodo * nuevoNodo= (Nodo *)malloc(sizeof(Nodo));
     nuevoNodo->T.TareaID=tarea;
     nuevoNodo->T.Duracion=duracion;
-    /*char buffer[MAX];*/
+    nuevoNodo->T.Descripcion=descripcion;
     nuevoNodo->Siguiente=NULL;
 
     return nuevoNodo;
+}
+
+void InsertarNodo(Nodo ** cabeza, Nodo *nuevoNodo){
+    nuevoNodo->Siguiente= *cabeza;
+    *cabeza=nuevoNodo;
 }
